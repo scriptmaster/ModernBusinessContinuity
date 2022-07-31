dfda4109519fb978279116ac88e38acbaf9f2e44
msheriffusa <msheriffusa@gmail.com>
2022-07-31 21:59:35 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+
+namespace ModernBusinessContinuity
+{
+    public class Program
+    {
+        static Config config;
+        static void Main(string[] args)
+        {
+            string configFile;
+            if (args.Length > 1)
+            {
+                configFile = args[0];
+            }
+            else
+            {
+                configFile = "config.json";
+            }
+
+            if (!File.Exists(configFile))
+            {
+                Console.WriteLine("Creating default config file");
+                config = new Config();
+
+                var writeNewConfig = JsonSerializer.Serialize<Config>(config);
+                File.WriteAllText(configFile, writeNewConfig);
+            }
+
+            try
+            {
+                var jsonText = File.ReadAllText(configFile);
+                config = JsonSerializer.Deserialize<Config>(jsonText) ?? new Config();
+            }
+            catch(Exception ex)
+            {
+                // 
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Config
+    {
+        public string SourceDirs { get; set; } = "src/";
+        public string main { get; set; } = "முதல்.அ.md";
+    }
+}
